aacabc3826129c36946ebd663f3f7199e724da27
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-04-20 22:34:07 -0500

    refactor launchApp

--- src/test/java/org/djd/BaseTest.java
+++ src/test/java/org/djd/BaseTest.java
@@ -2,8 +2,12 @@ package org.djd;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.testng.annotations.*;
 
 import java.io.FileReader;
@@ -13,24 +17,11 @@ import java.util.Properties;
 public class BaseTest {
     protected WebDriver driver;
     private Properties props;
-
-    // One way to set up browser and baseURL. Could also use TestNG parameters
-    public void parseProps() throws IOException {
-        // In IntelliJ, it's like the project/module directory
-        String currentDirectory = System.getProperty("user.dir"); // C:\Users\david\coding\java\Udemy_Practice\SeleniumE2E
-        props = new Properties();
-        FileReader reader = new FileReader(currentDirectory + "\\src\\test\\java\\org\\djd\\Test.properties"); // Double backslash because single backslash is escape character
-        props.load(reader);
-    }
-
-    @BeforeMethod(alwaysRun = true) // Always run so don't get skipped over if using Groups
+    
+    @BeforeMethod(alwaysRun = true) // Always run so don't get skipped over if using TestNG Groups
     public void launchApp() throws IOException {
-        parseProps();
-        String browser = props.getProperty("browser");
-        if (browser.equalsIgnoreCase("Chrome")) {driver = new ChromeDriver();}
-        else if (browser.equalsIgnoreCase("Edge")) {driver = new EdgeDriver();}
-        else if (browser.equalsIgnoreCase("Firefox")) {driver = new FirefoxDriver();}
-        driver.manage().window().maximize();
+        parseConfig();
+        setUpDriver();
         try {
             driver.get(props.getProperty("testURL"));
         } catch (Exception e) {
@@ -41,5 +32,28 @@ public class BaseTest {
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit(); // Nulls the driver object
+    }
+
+    // One way to set up browser and baseURL. Could also use TestNG parameters
+    private void parseConfig() throws IOException {
+        // In IntelliJ, it's like the project/module directory
+        String currentDirectory = System.getProperty("user.dir"); // C:\Users\david\coding\java\Udemy_Practice\SeleniumE2E
+        props = new Properties();
+        FileReader reader = new FileReader(currentDirectory + "\\src\\test\\java\\org\\djd\\Test.properties"); // Double backslash because single backslash is escape character
+        props.load(reader);
+    }
+
+    private void setUpDriver() {
+        String browser = props.getProperty("browser");
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        }
+        else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        }
+        else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
     }
 }

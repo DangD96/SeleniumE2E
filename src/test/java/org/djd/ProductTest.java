26b319d8054739cde2fe93b5cbafb24b9ed81c9f
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-04-28 00:08:50 -0500

    remove unused imports

--- src/test/java/org/djd/ProductTest.java
+++ src/test/java/org/djd/ProductTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ProductTest extends BaseTest{
+public class ProductTest extends BaseTest {
     LoginPage loginPage;
     CatalogPage catalogPage;
     ShoppingCart cart;

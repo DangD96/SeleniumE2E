83a775b06722a531cfd6da408d4f3df0c6d06a4d
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-05-02 21:50:56 -0500

    move page objects out of package

--- src/main/java/org/djd/BasePage.java
+++ src/main/java/org/djd/BasePage.java
@@ -1,4 +1,4 @@
-package org.djd.pages;
+package org.djd;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/main/java/org/djd/LoginPage.java
+++ src/main/java/org/djd/LoginPage.java
@@ -1,4 +1,4 @@
-package org.djd.pages;
+package org.djd;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;

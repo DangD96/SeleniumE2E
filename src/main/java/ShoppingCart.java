061d3e3e1e18c9733a037b9ab6076a87ea907618
DangD96 <35606061+DangD96@user.noreply.github.com>
2024-12-24 15:59:51 -0600

    update page object structure

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,31 +1,33 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ShoppingCart extends BasePage {
+    public ShoppingCart(WebDriver driver) {super(driver);}
+
+    // region Locators
     final By SHOPPING_CART = By.tagName("table");
     final By PRODUCTS_IN_CART = By.cssSelector("tr:has(td[class*='col-sm'])");
+    public WebElement getShoppingCart() {return waitForElementToBeVisible(SHOPPING_CART);}
+    public List<WebElement> getAllProducts() {return waitForElementsToBeVisible(PRODUCTS_IN_CART);}
 
-    public ShoppingCart(WebDriver driver) {
-        super(driver);
-    }
+    // endregion
 
+    // region Performers
+    public void removeProduct(int row) {getAllProducts().get(row-1).findElement(By.cssSelector("td button")).click();}
+    // endregion
+
+
+    // region Getters
     public int getNumberOfProductsInCart() {
-        try {
-            return waitForElementsToBeVisible(PRODUCTS_IN_CART).size();
-        }
-        catch (Exception e) {
-            return 0;
-        }
+        try {return getAllProducts().size();}
+        catch (Exception e) {return 0;}
     }
 
     public String getProductName(int row) {
-        return waitForElementsToBeVisible(PRODUCTS_IN_CART).get(row-1)
-                .findElement(By.cssSelector("td .media-body h4.media-heading"))
-                .getText();
+        return getAllProducts().get(row-1).findElement(By.cssSelector("td .media-body h4.media-heading")).getText();
     }
-
-    public void removeProduct(int row) {
-        waitForElementsToBeVisible(PRODUCTS_IN_CART).get(row-1)
-                .findElement(By.cssSelector("td button")).click();
-    }
+    // endregion
 }

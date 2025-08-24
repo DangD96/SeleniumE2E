31b233cda5e976ee157986b0ea2a42d2139e7f59
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-08-24 14:40:25 -0500

    add more action methods

--- src/main/java/framework/BasePage.java
+++ src/main/java/framework/BasePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -22,6 +21,25 @@ public abstract class BasePage {
         this.js = (JavascriptExecutor) driver;
         this.WAIT = new WebDriverWait(driver, Duration.ofSeconds(5));
         WaitForAJAX();
+    }
+
+    // Function = takes something, returns something
+    public <R> R ActionWithRetry(By locator, Function<By, R> function, boolean isInteractiveAction) {
+        int retries = 0;
+        while (retries < MAX_RETRIES) {
+            try {
+                if (isInteractiveAction) {
+                    WaitForElementToBeInteractable(locator); // for actions that need element to be interactable
+                } else {
+                    WaitForElementToBeVisible(locator); // for actions like that just need element to be visible
+                }
+                return function.apply(locator);
+            } catch (StaleElementReferenceException | ElementClickInterceptedException retry) {
+                retries++;
+                Sleep(WAIT_MS);
+            }
+        }
+        throw new RuntimeException(String.format("Action failed after %d retries for locator: %s", MAX_RETRIES, locator));
     }
 
     public void Click(By locator) {
@@ -45,23 +63,42 @@ public abstract class BasePage {
         return ActionWithRetry(locator, loc -> driver.findElement(loc).getText(), false);
     }
 
-    // Function = takes something, returns something
-    public <R> R ActionWithRetry(By locator, Function<By, R> function, boolean isInteractiveAction) {
-        int retries = 0;
-        while (retries < MAX_RETRIES) {
-            try {
-                if (isInteractiveAction) {
-                    WaitForElementToBeInteractable(locator); // for actions that need element to be interactable
-                } else {
-                    WaitForElementToBeVisible(locator); // for actions like that just need element to be visible
-                }
-                return function.apply(locator);
-            } catch (StaleElementReferenceException | ElementClickInterceptedException retry) {
-                retries++;
-                Sleep(WAIT_MS);
-            }
+    public int GetSize(By locator) {
+        return ActionWithRetry(locator, loc -> driver.findElements(loc).size(), false);
+    }
+
+    public Boolean IsDisplayed(By locator) {
+        return ActionWithRetry(locator, loc -> driver.findElement(loc).isDisplayed(), false);
+    }
+
+    public Boolean IsHidden(By locator) {
+        return !ActionWithRetry(locator, loc -> driver.findElement(loc).isDisplayed(), false);
+    }
+
+    public Boolean IsEnabled(By locator) {
+        return ActionWithRetry(locator, loc -> driver.findElement(loc).isEnabled(), false);
+    }
+
+    public Boolean IsDisabled(By locator) {
+        return !ActionWithRetry(locator, loc -> driver.findElement(loc).isEnabled(), false);
+    }
+
+    public Boolean IsExists(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
         }
-        throw new RuntimeException(String.format("Action failed after %d retries for locator: %s", MAX_RETRIES, locator));
+    }
+
+    public Boolean IsNotExist(By locator) {
+        try {
+            driver.findElement(locator);
+            return false;
+        } catch (NoSuchElementException pass) {
+            return true;
+        }
     }
 
     public void Sleep(long ms) {
@@ -72,20 +109,20 @@ public abstract class BasePage {
         }
     }
 
-    public WebElement WaitForElementToBeInteractable(By locator) {
-        return WAIT.until(ExpectedConditions.elementToBeClickable(locator));
+    public void WaitForElementToBeInteractable(By locator) {
+        WAIT.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
-    public WebElement WaitForElementToBeVisible(By locator) {
-        return WAIT.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    public void WaitForElementToBeVisible(By locator) {
+        WAIT.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    public List<WebElement> WaitForElementsToBeVisible(By locator) {
-        return WAIT.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    public void WaitForElementsToBeVisible(By locator) {
+        WAIT.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
     }
 
-    public boolean WaitForElementToBeInvisible(By locator) {
-        return WAIT.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    public void WaitForElementToBeInvisible(By locator) {
+        WAIT.until(ExpectedConditions.invisibilityOfElementLocated(locator));
     }
 
     public void WaitForAJAX() {
@@ -112,5 +149,14 @@ public abstract class BasePage {
 
     public String GetURL() {
         return driver.getCurrentUrl();
+    }
+
+    // to help with building locators via composition (parent-child elements in repeatable UI)
+    public By AppendToXpath(By baseLocator, String relative) {
+        if (!baseLocator.toString().startsWith("By.xpath: ")) {
+            throw new IllegalArgumentException("This method only supports XPath By objects");
+        }
+        String xpathAsString = baseLocator.toString().replace("By.xpath: ", "");
+        return By.xpath(xpathAsString + relative);
     }
 }

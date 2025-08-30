c8182a9504fc732f3b04d7fa2cc452cc18d77de1
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-08-30 17:39:54 -0500

    add templates

---
+++ src/main/java/framework/templates/TemplatePage.java
@@ -0,0 +1,38 @@
+package framework.templates;
+
+import framework.Is;
+import framework.SparkDriver;
+import org.openqa.selenium.By;
+
+import static framework.Utilities.appendToXpath;
+
+public class TemplatePage {
+    // region Locators
+    public static By myBtn() {return By.id("btn123");}
+
+    public static By myProduct(String name) {return By.xpath(String.format("//app-card[.//*[@class='card-title']//a[text()='%s']]", name));}
+
+    public static By myProductPrice(String name) {return appendToXpath(myProduct(name), "//h5");}
+    // endregion
+
+
+    // region Performers
+    public static void clickMyBtn() {
+        SparkDriver.click(myBtn());
+    }
+    // endregion
+
+
+    // region Getters
+    public static String getMyProductPrice(String name) {
+        return SparkDriver.getText(myProductPrice(name));
+    }
+    // endregion
+
+
+    // region Booleans
+    public static Boolean isMyBtnEnabled() {
+        return Is.enabled(myBtn());
+    }
+    // endregion
+}
---
+++ src/main/java/framework/templates/TemplateTest.java
@@ -0,0 +1,11 @@
+package framework.templates;
+
+import framework.SparkTest;
+import org.testng.annotations.Test;
+
+public class TemplateTest extends SparkTest {
+    @Test(description = "asdf")
+    public static void myTest() {
+
+    }
+}
